c0aed7378bf136524402dcfec724c82c0255f832
Mohammedsamman1 <mmmode.12321@gmail.com>
2026-07-01 21:53:54 +0300

    اضافة الابحاث الى admin dashboard

--- research/admin.py
+++ research/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from research.models import ResearchPaper, PlagiarismReport
 
-# Register your models here.
+
+@admin.register(ResearchPaper)
+class ResearchPaperAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'status', 'created_at')
+    list_editable = ('status',)
+    list_filter = ('status',)
+    search_fields = ('title', 'author__email')
+
+
+@admin.register(PlagiarismReport)
+class PlagiarismReportAdmin(admin.ModelAdmin):
+    list_display = ('paper', 'total_similarity_score', 'checked_at')
